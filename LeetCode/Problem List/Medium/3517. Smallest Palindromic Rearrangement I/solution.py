3159ce80ccc2f6c200e1e3131bc33b41c68bbe88
Igor Kozlov <kukara4a4a4@gmail.com>
2026-07-28 10:51:28 +0300

    code cleanup

--- LeetCode/Problem List/Medium/3517. Smallest Palindromic Rearrangement I/solution.py
+++ LeetCode/Problem List/Medium/3517. Smallest Palindromic Rearrangement I/solution.py
@@ -4,16 +4,16 @@ class Solution:
         list_s: list[str] = list(s)
         if len(s) <= 1:
             return s
+        
+        result.extend(list_s[:int((len(s) / 2))])
+        result.sort()
+
         if len(s) % 2 == 0:
-            result.extend(list_s[:int((len(s) / 2))])
-            result.sort()
             result.extend(result[::-1])
         else:
-            result.extend(list_s[:int((len(s) / 2))])
-            result.sort()
-            mid_result: list[str] = result[::-1]
-            result.append(list_s[int((len(s) // 2))])
-            result.extend(mid_result)
+            second_half: list[str] = result[::-1]
+            result.append(list_s[int((len(s) / 2))])
+            result.extend(second_half)
         return ''.join(result)
 
 
