dfe60bc25ab4c2dc2c5f014962d9e0af4431f6a7
Igor Kozlov <kukara4a4a4@gmail.com>
2026-09-07 11:28:06 +0300

    Solution to the problem 'Number of Elapsed Seconds Between Two Times'

---
+++ LeetCode/Problem List/Easy/3986. Number of Elapsed Seconds Between Two Times/solution.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+class Solution:
+    def secondsBetweenTimes(self, startTime: str, endTime: str) -> int:
+        return (
+            datetime.strptime(endTime, "%H:%M:%S") - 
+            datetime.strptime(endTime, "%H:%M:%S")
+        ).seconds
+
+startTime = "01:00:00"
+endTime = "02:00:25"
+
+test = Solution()
+print(test.secondsBetweenTimes(startTime=startTime, endTime=endTime))
