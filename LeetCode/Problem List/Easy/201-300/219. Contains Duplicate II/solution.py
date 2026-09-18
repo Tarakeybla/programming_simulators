094905699bbaf6d2540f0e8dbf99cbace8e9aceb
Igor Kozlov <kukara4a4a4@gmail.com>
2026-09-18 13:45:23 +0300

    update: Solution to the problem 'Contains Duplicate II'

--- LeetCode/Problem List/Easy/201-300/219. Contains Duplicate II/solution.py
+++ LeetCode/Problem List/Easy/201-300/219. Contains Duplicate II/solution.py
@@ -2,9 +2,7 @@ class Solution:
     def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
         if len(set(nums)) == len(nums):
             return False
-
         DIGIT_MAP = {}
-
         for index, value in enumerate(nums):
             if value not in DIGIT_MAP:
                 DIGIT_MAP[value] = {
@@ -14,7 +12,6 @@ class Solution:
             else:
                 DIGIT_MAP[value]['index'].append(index)
                 DIGIT_MAP[value]['count'] += 1
-
         for item in DIGIT_MAP.items():
             if item[1]['count'] >= 2:
                 indexs = item[1]['index']
@@ -23,5 +20,33 @@ class Solution:
                         if abs(indexs[i] - indexs[j]) <= k:
                             return True
                         break
-
         return False
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        if len(set(nums)) == len(nums):
+            return False
+        LAST_INDEX_MAP = dict()
+        for index, value in enumerate(nums):
+            if value in LAST_INDEX_MAP:
+                if index - LAST_INDEX_MAP[value] <= k:
+                    return True
+            LAST_INDEX_MAP[value] = index
+        return False
+
+
+nums = [1,0,1,1]
+k = 1
+
+test = Solution()
+print(test.containsNearbyDuplicate(nums, k))
+
+
+# Варианты формирования словаря:
+            # if value not in DIGIT_MAP:
+            #     DIGIT_MAP[value] = [index]
+            # else:
+            #     DIGIT_MAP[value].append(index)
+
+            # DIGIT_MAP.setdefault(value, []).append(index)
